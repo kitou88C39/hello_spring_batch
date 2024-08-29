a03fc18b2b4541b05d5087c5e0c34a05ad26c463
kitoumasaomi <kanfutrooper@gmail.com>
2024-08-29 20:21:19 +0900

    config/SpringConfig.javaを作成する

--- src/main/java/com.udemy.hello/config/SpringConfig.java
+++ src/main/java/com.udemy.hello/config/SpringConfig.java
@@ -3,6 +3,8 @@ package com.udemy.hello.config;
 import java.util.ArrayList;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
 
@@ -11,6 +13,10 @@ public class SpringConfig {
     private final JobLauncher jobLauncher;
     private final JobRepository jobRepository;
     private final PlatformTransactionManager platformTransactionManager;
+
+    @Autowired
+    @Qualifier("HelloTasklet1")
+    private Tasklet HelloTasklet1;
 
     public SpringConfig(JobLauncher jobLauncher, JobRepository jobRepository,
             PlatformTransactionManager transactionManager) {
