773d2a408544a8ab26f187f56a88a25300cdcfaf
kitoumasaomi <kanfutrooper@gmail.com>
2024-08-29 20:10:48 +0900

    tasklet/SpringConfig.javaを作成する

--- src/main/java/com.udemy.hello/tasklet/HelloTasklet1.java
+++ src/main/java/com.udemy.hello/tasklet/HelloTasklet1.java
@@ -1,10 +1,14 @@
 package com.udemy.hello.tasklet;
 
 import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.stereotype.Component;
 
+@Component("HelloTasklet1")
+@StepScope
 public class HelloTasklet1 implements Tasklet {
     @Override
     public RepeatStatus execute(StepCounteribution contribution,
