ed4896df040a71bf371b93cb54d0a151596bae1f
kitoumasaomi <kanfutrooper@gmail.com>
2024-08-29 11:51:03 +0900

    java/com/becoder/Enote_spring_bootにcontrollerパッケージHomeController.javaを作成する

--- src/main/java/com/udemy/hello/Config/SpringConfig.java
+++ src/main/java/com/udemy/hello/Config/SpringConfig.java
@@ -2,10 +2,16 @@ package com.udemy.hello.config;
 
 import java.util.ArrayList;
 
+import org.springframework.batch.core.launch.JobLauncher;
+
 @Configuration
 public class SpringConfig {
     private final JobLauncher jobLauncher;
     private final JobRepository jobRepository;
     private final PlatformTransactionManager platformTransactionManager;
+
+    public SpringConfig(JobLauncher jobLauncher JobRepository jobRepository PlatformTransactionManager transactionManager){
+
+    }
 
 }
