efb1e4e80b5e836f3db845934269105a4cd26cfa
kitoumasaomi <kanfutrooper@gmail.com>
2024-08-29 19:56:41 +0900

    tasklet/SpringConfig.javaを作成する

--- src/main/java/com.udemy.hello/tasklet/HelloTasklet1.java
+++ src/main/java/com.udemy.hello/tasklet/HelloTasklet1.java
@@ -1,13 +1,14 @@
 package com.udemy.hello.tasklet;
 
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 
-import io.micrometer.core.instrument.step.StepCounter;
-
 public class HelloTasklet1 implements Tasklet {
     @Override
-    public RepeatStatus execute(StepCounteribution contribution, )
-    return null;
-
+    public RepeatStatus execute(StepCounteribution contribution,
+            ChunkContext chunkContext) throws Exception {
+        return null;
+    }
 }
