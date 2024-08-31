0bb08c34cd9167853973f4d85698895f79233d23
kitoumasaomi <kanfutrooper@gmail.com>
2024-08-31 17:12:41 +0900

    validator/HelloJobParametersValidator.javaを作成する

--- src/main/java/com.udemy.hello/validator/HelloJobParametersValidator.java
+++ src/main/java/com.udemy.hello/validator/HelloJobParametersValidator.java
@@ -1,7 +1,13 @@
 package com.udemy.hello.validator;
 
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersInvalidException;
 import org.springframework.batch.core.JobParametersValidator;
 
 public class HelloJobParametersValidator implements JobParametersValidator {
+
+    @Override
+    public void validate(JobParameters parameters) throws JobParametersInvalidException {
+    }
 
 }
