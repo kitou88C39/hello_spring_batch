835028b20318baf9e7749078d71cbc0f5c821e31
kitoumasaomi <kanfutrooper@gmail.com>
2024-09-01 17:28:48 +0900

    tasklet/HelloTasklet2.javaを作成する

--- src/main/java/com.udemy.hello/tasklet/HelloTasklet2.java
+++ src/main/java/com.udemy.hello/tasklet/HelloTasklet2.java
@@ -1,9 +1,12 @@
 package com.udemy.hello.tasklet;
 
 import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 @Component("HelloTasklet2")
 @StepScope
