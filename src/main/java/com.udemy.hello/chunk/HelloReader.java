18061e3fa44b9135f2b64eb0cbc4092844c5076d
kitoumasaomi <kanfutrooper@gmail.com>
2024-09-03 14:14:48 +0900

    chunk/HelloReader.javaを作成する

--- src/main/java/com.udemy.hello/chunk/HelloReader.java
+++ src/main/java/com.udemy.hello/chunk/HelloReader.java
@@ -1,7 +1,16 @@
 package com.udemy.hello.chunk;
 
+import java.nio.channels.NonReadableChannelException;
+import java.text.ParseException;
+
+import org.springframework.batch.item.UnexpectedInputException;
+
 public class HelloReader implements IntemReader<String> {
 
     private int intIndex = 0;
+
+    public String read() throws Exception, UnexpectedInputException,
+            ParseException, NonReadableChannelException {
+    }
 
 }
