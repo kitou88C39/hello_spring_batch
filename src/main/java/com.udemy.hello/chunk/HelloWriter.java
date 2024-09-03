d50e1766a0d90257fd5152eb25b332c0f610b31e
kitoumasaomi <kanfutrooper@gmail.com>
2024-09-03 15:51:17 +0900

    chunk/HelloWriter.javaを作成する

--- src/main/java/com.udemy.hello/chunk/HelloWriter.java
+++ src/main/java/com.udemy.hello/chunk/HelloWriter.java
@@ -1,7 +1,15 @@
 package com.udemy.hello.chunk;
 
+import org.springframework.batch.item.Chunk;
 import org.springframework.batch.item.ItemWriter;
 
 public class HelloWriter implements ItemWriter<String> {
+
+    @Override
+    public void write(Chunk<? extends String> chunk)
+            throws Exception {
+        log.info("Writer chunk={}", chunk);
+        log.info("-----------------");
+    }
 
 }
