6f9356b6a125625591e55aa811ee96d6da13b8ef
kitoumasaomi <kanfutrooper@gmail.com>
2024-09-03 20:21:48 +0900

    listener/HelloJobExecutionListener.javaを作成する

--- src/main/java/com.udemy.hello/listener/HelloJobExecutionListener.java
+++ src/main/java/com.udemy.hello/listener/HelloJobExecutionListener.java
@@ -4,9 +4,15 @@ import org.springframework.batch.core.JobExecutionListener;
 
 public class HelloJobExecutionListener implements JobExecutionListener {
 
+    @Override
     public void beforeJob(JobExecution jobExecution) {
 
         JobExecutionListener.super.beforeJob(jobExecution);
     }
 
+    @Override
+    public void beforeJob(JobExecution jobExecution) {
+
+        JobExecutionListener.super.beforeJob(jobExecution);
+    }
 }
