43e37a83a7eae58ce4d24a630b6ae991a7cd1058
kitoumasaomi <kanfutrooper@gmail.com>
2024-09-05 16:34:09 +0900

    src/main/resoucesにmodel/Employee.javaを作成する

--- src/main/java/com.udemy.hello/model/Employee.java
+++ src/main/java/com.udemy.hello/model/Employee.java
@@ -3,9 +3,9 @@ package com.udemy.hello.model;
 public class Employee {
 
     private Integer empNumber;
-    private Integer empNumber;
-    private Integer empNumber;
-    private Integer empNumber;
-    private Integer empNumber;
+    private String empName;
+    private String jobTitle;
+    private Integer mgrNumber;
+    private Date hireDate;
 
 }
