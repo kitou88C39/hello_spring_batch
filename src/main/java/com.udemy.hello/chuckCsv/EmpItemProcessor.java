02375ca72f9256176712e93599f5db2a4017b38c
kitoumasaomi <kanfutrooper@gmail.com>
2024-09-06 15:44:09 +0900

    chuckCsv/EmpItemProcessor.javaを作成する

--- src/main/java/com.udemy.hello/chuckCsv/EmpItemProcessor.java
+++ src/main/java/com.udemy.hello/chuckCsv/EmpItemProcessor.java
@@ -1,13 +1,19 @@
 package com.udemy.hello.chuckCsv;
 
 import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.item.ItemProcessor;
 import org.springframework.stereotype.Component;
+
+import com.udemy.hello.model.Employee;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Component("EmpItemProcessor")
 @StepScope
 @Slf4j
-public class EmpItemProcessor {
+public class EmpItemProcessor implements ItemProcessor<Employee, Employee> {
+    public Employee process(Employee item) throws Exception {
+        return null;
+    }
 
 }
