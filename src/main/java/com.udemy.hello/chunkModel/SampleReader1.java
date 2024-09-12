81516ccb42133bf649645766e6d047ba4ac7b944
kitoumasaomi <kanfutrooper@gmail.com>
2024-09-12 21:44:21 +0900

    chunkModelパッケージSampleReader1.javaを作成する

--- src/main/java/com.udemy.hello/chunkModel/SampleReader1.java
+++ src/main/java/com.udemy.hello/chunkModel/SampleReader1.java
@@ -6,4 +6,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class SampleReader1 implements ItemReader<ReceiveFileInfo> {
 
+    List<String> lines = null;
+    int currentIndex = 0;
+
 }
