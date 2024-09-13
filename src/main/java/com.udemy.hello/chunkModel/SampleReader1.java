5222c162081f62456ad8adbb6ebdcad9eda028c6
kitoumasaomi <kanfutrooper@gmail.com>
2024-09-13 10:46:49 +0900

    chunkModelパッケージSampleReader1.javaを作成する

--- src/main/java/com.udemy.hello/chunkModel/SampleReader1.java
+++ src/main/java/com.udemy.hello/chunkModel/SampleReader1.java
@@ -1,6 +1,9 @@
 package com.udemy.hello.chunkModel;
 
+import java.text.ParseException;
+
 import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.UnexpectedInputException;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -9,4 +12,9 @@ public class SampleReader1 implements ItemReader<ReceiveFileInfo> {
     List<String> lines = null;
     int currentIndex = 0;
 
+    @Override
+    public ReceiverFileInfo read() {
+        throw Exception, UnexpectedInputException, ParseException, NonTransientResourceException{}
+    
+    }
 }
