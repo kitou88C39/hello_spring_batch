38b7fecf7f7831cb351838261c7513bb27c4b33c
kitoumasaomi <kanfutrooper@gmail.com>
2024-09-13 16:01:52 +0900

    chunkModel/SampleProcessor.javaを作成する

--- src/main/java/com.udemy.hello/chunkModel/SampleProcessor.java
+++ src/main/java/com.udemy.hello/chunkModel/SampleProcessor.java
@@ -10,9 +10,12 @@ public class SampleProcessor implements ItemProcessor<ReceiveFileInfo, FileInfo>
     public FileInfo process(ReceiveFileInfo item) throws Exception {
         var processedFileInfo = new FileInfo();
         processedFileInfo.setId(item.getId());
-        processedFileInfo.setId(item.getId());
-        processedFileInfo.setId(item.getId());
-        processedFileInfo.setId(item.getId());
+        processedFileInfo.setName(item.getName());
+        processedFileInfo.setAge(item.getAge());
+        processedFileInfo.setAdult(item.getAge() > 19);
+
+        return processedFileInfo;
+
     }
 
 }
