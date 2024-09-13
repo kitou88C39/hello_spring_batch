6ed394b4131fd09c2ac81d909eda12e789b05353
kitoumasaomi <kanfutrooper@gmail.com>
2024-09-13 19:33:06 +0900

    chunkModel/SampleWriter1.javaを作成する

--- src/main/java/com.udemy.hello/chunkModel/SampleWriter1.java
+++ src/main/java/com.udemy.hello/chunkModel/SampleWriter1.java
@@ -1,9 +1,14 @@
 package com.udemy.hello.chunkModel;
 
+import org.springframework.batch.item.Chunk;
 import org.springframework.batch.item.ItemWriter;
 
 public class SampleWriter1 implements ItemWriter<FileInfo> {
 
     private final FileRepository repository;
+
+    @Override
+    public void write(Chunk<? extends FileInfo> chunk) throws Exception {
+    }
 
 }
