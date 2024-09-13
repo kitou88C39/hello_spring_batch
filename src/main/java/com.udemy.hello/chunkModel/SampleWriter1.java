53a91fe29eb0e9c045cb542b48d20f8cead73d9d
kitoumasaomi <kanfutrooper@gmail.com>
2024-09-13 19:44:58 +0900

    chunkModel/SampleWriter1.javaを作成する

--- src/main/java/com.udemy.hello/chunkModel/SampleWriter1.java
+++ src/main/java/com.udemy.hello/chunkModel/SampleWriter1.java
@@ -1,9 +1,11 @@
 package com.udemy.hello.chunkModel;
 
-import org.apache.tomcat.jni.FileInfo;
 import org.springframework.batch.item.Chunk;
+import org.apache.tomcat.jni.FileInfo;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.stereotype.Component;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import lombok.RequiredArgsConstructor;
 
